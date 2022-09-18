0505a656fe3ef8835a1b2f106d4e4df60c6f6bd2
Reyzu <reyzuproject@gmail.com>
2022-09-18 09:52:43 +0700

    Update cust_filters.py

--- ReyzuRobot/modules/cust_filters.py
+++ ReyzuRobot/modules/cust_filters.py
@@ -610,12 +610,12 @@ __help__ = """
 ❧ /filters - List all active filters saved in the chat.
 
 *Admin only :*
-❧ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
-is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
+❧ /filter <keyword> <reply message> - Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE : all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
 Separate diff replies by `%%%` to get random replies
-*Example:* 
+*Example :* 
 `/filter "filtername"
 Reply 1
 %%%
@@ -629,7 +629,6 @@ Reply 3`
 
 ❧ /removeallfilters *:* Remove all chat filters at once.
 *Note* : Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
-Check /markdownhelp to know more!
 """
 
 __mod_name__ = "Filters"
